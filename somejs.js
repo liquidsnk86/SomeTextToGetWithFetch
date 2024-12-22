0d98f80fb2d2ffc18a6c849b2eed21f2dcd22d99
Santiago Herrera <150970732+liquidsnk86@users.noreply.github.com>
2024-12-22 04:35:15 -0300

    Update somejs.js

--- somejs.js
+++ somejs.js
@@ -3,8 +3,11 @@
         "https://calcagni-gabriel.vercel.app/api/non-followers?user=solidsnk86"
       );
       const jsonData = await response.json();
-      const nonFollowingCount = jsonData.data.nonfollowings_count
+      const nonFollowingCount = jsonData.data.nonfollowings_count;
       document.body.textContent = `No le siguen: ${nonFollowingCount}`;
-      const obj = {following: "no_seguidores", count: nonFollowingCount}
-      localStorage.setItem(JSON.stringify(obj));
+      const obj = {
+            following: "no_seguidores",
+            count: nonFollowingCount
+      };
+      localStorage.setItem(JSON.parse(obj));
 })();
