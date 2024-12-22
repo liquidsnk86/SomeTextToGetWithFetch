707e0cba901b39671f22e916249fd03f65389ac8
Santiago Herrera <150970732+liquidsnk86@users.noreply.github.com>
2024-12-22 06:28:30 -0300

    Update somejs.js

--- somejs.js
+++ somejs.js
@@ -3,15 +3,17 @@
         "https://calcagni-gabriel.vercel.app/api/non-followers?user=solidsnk86"
       );
       const jsonData = await response.json();
+      
       const nonFollowingCount = jsonData.data.nonfollowings_count;
       document.body.textContent = `No le siguen: ${nonFollowingCount}`;
-      window.localStorage?.setItem("no_seguidores", nonFollowingCount);
+      const 
+      window.localStorage?.setItem("github-stats", JSON.stringify({ no_siguen: nonFollowingCount }));
       if(navigator.geolocation) {
             await navigator.geolocation.getCurrentPosition((position) => {
                   const latitude = position.coords.latitude;
                   const longitude = position.coords.longitude;
                   const obj = { latitud: latitude, longitud: longitude };
-                  window.localStorage?.setItem("data", JSON.stringify(obj));
+                  window.localStorage?.setItem("ubicación", JSON.stringify(obj));
       });
       }
 })();
